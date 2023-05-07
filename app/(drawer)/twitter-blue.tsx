77f2dee3a9f74fddcb8e2fee50e3a6f75fe1aaa1
Wilson Lopez <wiltrouble@gmail.com>
2023-05-07 11:54:05 -0400

    Added Drawer navigator

---
+++ app/(drawer)/twitter-blue.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TwitterBlue = () => {
+  return (
+    <View>
+      <Text>TwitterBlue</Text>
+    </View>
+  )
+}
+
+export default TwitterBlue
+
+const styles = StyleSheet.create({})
